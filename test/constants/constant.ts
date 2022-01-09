7f8a81effdafe9396447b06ce004617f51379b37
rebeccali-sage <rebecca.li@sage.com>
2022-01-09 16:17:13 +0100

    Revert "first commit, init wdio project"
    This reverts commit de32889c1fa14a17af3b7ea00bc27869e334bf33.

--- test/constants/constant.ts
+++
@@ -1,4 +0,0 @@
-export const INVALID_LOGIN_PASSWORD_MSG = 'Invalid login or password.';
-export const REQUIRED_FIELD_ERROR = 'This is a required field.';
-export const LOGIN_TITLE = "Customer Login";
-export const LANDING_TITLE = "Madison Island";
--- test/pageobjects/page.js
+++
@@ -1,7 +0,0 @@
-
-export default class Page {
-
-    url(path="") {
-        return `${browser.options.baseUrl}${path}`;
-    }
-}
